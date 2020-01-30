315e055fe3a7486363b5e3b24c8be8961d85f26e
MJU <marie.ju2012@outlook.com>
2020-01-30 19:22:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if !(int % 3 == 0 || int % 5 == 0)
-    puts "nil"
+  if (int % 3 == 0 and int % 5 == 0)
+    puts "fizzbuzz"
   elsif int % 3 == 0
     puts "Fizz"
   elsif int % 5 == 0
     puts "Buss"
   else
-    puts "FizzBuss"
+    puts "nil"
   end
 end
 fizzbuzz(3)
